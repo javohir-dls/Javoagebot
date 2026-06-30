bacdcf37e640749e2810b56aed455ef434e0a770
javohir-dls <chorikichinov21@gmail.com>
2026-06-30 14:15:51 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+# config.py
 
+BOT_TOKEN = "8713752955:AAFXESPsLR7I5XCVkm50y0Ng82CUwlRt7Ks"
+
+CHANNEL_USERNAME = "@xushboqovblog"
+CHANNEL_URL = "https://t.me/xushboqovblog"
+
+INSTAGRAM_USERNAME = "@javohir.ftbl"
+INSTAGRAM_URL = "https://instagram.com/javohir.ftbl"
+
+BOT_NAME = "Age Calculator"
+BOT_USERNAME = "@JavoAgeBot"
